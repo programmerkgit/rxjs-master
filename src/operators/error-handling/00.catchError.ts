fa2d41bbcd99ae8924cff76cbbda0eabcb64a47e
admin <k.kubo.private.mobile@gmail.com>
2020-04-11 03:58:50 +0900

    error

--- src/operators/error-handling/00.catchError.ts
+++ src/operators/error-handling/00.catchError.ts
@@ -1,5 +1,5 @@
 import { of } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 of(1, 2, 3, 4, 5).pipe(
     map(n => {
@@ -12,3 +12,19 @@ of(1, 2, 3, 4, 5).pipe(
 )
     .subscribe(x => console.log(x));
 // 1, 2, 3, I, II, III, IV, V
+
+of(1, 2, 3, 4, 5).pipe(
+    map(n => {
+        if (n === 4) {
+            throw new Error('four!');
+        }
+        return n;
+    }),
+).subscribe({
+    next(v: any) {
+        console.log(v);
+    },
+    error(e: any) {
+        console.error(e.toString(), 'toString')
+    }
+});
